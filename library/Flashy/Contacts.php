270c3449ed5188959d152fed3749e29f35215c41
CBox <support@cbox.co.il>
2020-06-07 07:43:38 +0300

    Update Contacts.php

--- library/Flashy/Contacts.php
+++ library/Flashy/Contacts.php
@@ -47,6 +47,44 @@ class Flashy_Contacts {
 
     /**
      * Create contact
+     * @param struct $contact
+     *     - email string required valid email address.
+     *     - phone string of contact phone number.
+     *     - first_name string of recipient phone number.
+     *     - last_name string text message that will be sent
+     *     - source string contact source.
+     * @return array of structs 
+     *     - return[] struct the sending results for a single recipient
+     *         - success boolean true / false
+     *         - errors array error list
+     *         - contact array if the contact created successfully
+     */
+    public function createOrUpdate($contact)
+    {
+        if( isset($_COOKIE['flashy_attribution']) && $_COOKIE['flashy_attribution'] != "" )
+        {
+            $attr = implode(">", json_decode($_COOKIE['flashy_attribution']));
+
+            $contact['attribution'] = $attr;
+        }
+
+        $_params = array("contact" => $contact);
+
+        $contact = $this->master->call('contacts/create-or-update', $_params);
+
+        // If we created the contact successfully we will also send all the events history of the contact + setCustomer.
+        if( isset($contact['success']) && $contact['success'] == true )
+        {
+            $this->master->events->bulk($contact['contact']['contact_id']);
+
+            $this->master->events->setCustomer($contact['contact']['email']);
+        }
+
+        return $contact;
+    }
+
+    /**
+     * Create contact
      * @param string $email email address of the contact we want to update
      * @param struct $contact
      *     - phone string of contact phone number.
